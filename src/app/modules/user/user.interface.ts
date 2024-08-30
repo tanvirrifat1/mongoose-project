57937350d00b8a6cce9e1cbc32b1622eeccec82d
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2024-08-30 19:43:22 +0800

    added user interface

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,8 @@
+export type TUser = {
+  id: string;
+  password: string;
+  needsPasswordChange: boolean;
+  role: 'admin' | 'faculty' | 'student';
+  status: 'in-progress' | 'blocked';
+  isDeleted: boolean;
+};
