d349facdcd7ec3f6184449c5e913759de7f7300c
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2024-08-30 20:51:06 +0800

    added validation

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,7 +1,7 @@
 export type TUser = {
   id: string;
   password: string;
-  needsPasswordChange: boolean;
+  needsPasswordChange?: boolean;
   role: 'admin' | 'faculty' | 'student';
   status: 'in-progress' | 'blocked';
   isDeleted: boolean;
---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+
+const userValidationSchema = z.object({
+  id: z.string({ required_error: 'ID is required' }),
+  password: z
+    .string()
+    .max(25, { message: 'Password should have more then 25 character ' }),
+  needsPasswordChange: z.boolean().optional(),
+  role: z.enum(['admin', 'faculty', 'student'], {
+    errorMap: () => ({ message: 'role must be admin or faculty or student' }),
+  }),
+  status: z.enum(['in-progress', 'blocked'], {
+    errorMap: () => ({ message: 'role must be in-progress, blocked' }),
+  }),
+  isDeleted: z.boolean(),
+});
+
+export const UserValidation = {
+  userValidationSchema,
+};
