9ffb45f150777e42a909a516d34d023fed161e4f
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2024-09-18 21:49:29 +0800

    added more

--- src/app/modules/admin/admin.interface.ts
+++ src/app/modules/admin/admin.interface.ts
@@ -1,8 +1,6 @@
 import { Model, Types } from 'mongoose';
 
 export type TGender = 'male' | 'female' | 'other';
-
-
 export type TBloodGroup =
   | 'A+'
   | 'A-'
@@ -19,7 +17,7 @@ export type TUserName = {
   lastName: string;
 };
 
-export type TFaculty = {
+export type TAdmin = {
   id: string;
   user: Types.ObjectId;
   designation: string;
@@ -33,10 +31,9 @@ export type TFaculty = {
   presentAddress: string;
   permanentAddress: string;
   profileImg?: string;
-  academicDepartment: Types.ObjectId;
   isDeleted: boolean;
 };
 
-export interface FacultyModel extends Model<TFaculty> {
-  isUserExists(id: string): Promise<TFaculty | null>;
+export interface AdminModel extends Model<TAdmin> {
+  isUserExists(id: string): Promise<TAdmin | null>;
 }
--- src/app/modules/faculty/faculty.route.ts
+++ src/app/modules/faculty/faculty.route.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import { FacultyController } from './faculty.controller';
+import validateRequest from '../../middlewares/ValidateRequst';
+import { facultyValidations } from './faculty.validation';
 
 const router = express.Router();
 
 router.get('/', FacultyController.getAllFaculty);
 router.get('/:id', FacultyController.getSingleFaculty);
-router.patch('/:id', FacultyController.updateFaculty);
+router.patch(
+  '/:id',
+  validateRequest(facultyValidations.updateFacultyValidationSchema),
+  FacultyController.updateFaculty,
+);
 router.delete('/:id', FacultyController.deletedFaculty);
 
 export const FacultyRoute = router;
